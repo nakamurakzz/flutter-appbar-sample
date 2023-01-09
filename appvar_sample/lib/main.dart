a4f06c83648ddccbe1e57236189fcb55b143b6de
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-01-09 16:23:48 +0900

    色んなScafoldの小要素を追加

--- appvar_sample/lib/main.dart
+++ appvar_sample/lib/main.dart
@@ -11,6 +11,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Appbar Sample'),
+      ),
+      body: Text("Hello World"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      persistentFooterButtons: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.abc)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.safety_check)),
+      ],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                debugPrint("Item 1");
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                debugPrint("Item 2");
+              },
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(12),
@@ -19,12 +54,25 @@ class MyApp extends StatelessWidget {
               color: Colors.blueGrey,
               borderRadius: BorderRadius.all(Radius.circular(24))),
           child: Row(
-            children: const [
+            children: [
+              Spacer(),
               SizedBox(
                 width: 36,
                 height: 36,
-                child: Icon(Icons.search),
+                child:
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.abc)),
               ),
+              const SizedBox(
+                width: 36,
+                height: 36,
+                child: Icon(Icons.home),
+              ),
+              const SizedBox(
+                width: 36,
+                height: 36,
+                child: Icon(Icons.safety_check),
+              ),
+              Spacer(),
             ],
           ),
         ),
